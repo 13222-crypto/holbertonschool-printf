33a0374c34894e08306ee9b119d9fdc408c8bcca
reem <ryma6110@gmail.com>
2026-04-14 15:26:56 +0000

    Handle %d and %i conversion specifiers

--- _printf.c
+++ _printf.c
@@ -1,9 +1,33 @@
 #include "main.h"
 
 /**
+ * print_number - prints an integer
+ * @n: integer to print
+ * Return: number of characters printed
+ */
+int print_number(int n)
+{
+	unsigned int n1;
+	int count = 0;
+
+	if (n < 0)
+	{
+		count += _putchar('-');
+		n1 = -n;
+	}
+	else
+		n1 = n;
+
+	if (n1 / 10)
+		count += print_number(n1 / 10);
+
+	count += _putchar((n1 % 10) + '0');
+	return (count);
+}
+
+/**
  * _printf - produces output according to a format
  * @format: character string containing directives
- *
  * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
@@ -14,9 +38,8 @@ int _printf(const char *format, ...)
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
 	va_start(args, format);
-	while (format[i])
+	while (format && format[i])
 	{
 		if (format[i] == '%')
 		{
@@ -31,8 +54,10 @@ int _printf(const char *format, ...)
 				while (*str)
 					count += _putchar(*str++);
 			}
-			else if (format[i] == '%')
+			else if (format[i] == '%' )
 				count += _putchar('%');
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_number(va_arg(args, int));
 			else
 			{
 				count += _putchar('%');
