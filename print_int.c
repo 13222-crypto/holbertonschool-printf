8d6457a2b720a908fae7ec0e346de1e131d4fa25
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-17 23:40:49 +0000

    Fix: accuracy of return value in print_int and precision handling

--- print_int.c
+++ print_int.c
@@ -2,11 +2,6 @@
 
 /**
  * print_int - prints an integer with width and precision
- * @l: va_list
- * @mod: modifier
- * @width: width
- * @prec: precision
- * Return: count of chars printed
  */
 int print_int(va_list l, char mod, int width, int prec)
 {
@@ -19,19 +14,17 @@ int print_int(va_list l, char mod, int width, int prec)
 	else if (mod == 'h') n = (short int)va_arg(l, int);
 	else n = va_arg(l, int);
 
-	if (n < 0) {
-		num = -n;
-		negative = 1;
-	} else num = n;
+	if (n < 0) { num = -n; negative = 1; }
+	else num = n;
 
 	temp = num;
 	if (num == 0 && prec != 0) len = 1;
 	else while (temp > 0) { temp /= 10; len++; }
 
 	if (prec > len) zeros = prec - len;
-	
-	/* حساب المساحات: الإشارة + الأصفار + الأرقام */
-	if (n == 0 && prec == 0) len = 0;
+	if (num == 0 && prec == 0) len = 0;
+
+	/* طباعة المسافات للـ width */
 	while (width > (len + zeros + negative)) {
 		count += _putchar(' ');
 		width--;
@@ -40,12 +33,12 @@ int print_int(va_list l, char mod, int width, int prec)
 	if (negative) count += _putchar('-');
 	while (zeros-- > 0) count += _putchar('0');
 
-	if (!(n == 0 && prec == 0)) {
+	if (!(num == 0 && prec == 0)) {
 		temp = num;
-		while (num / div > 9) div *= 10;
+		while (temp / div > 9) div *= 10;
 		while (div != 0) {
-			count += _putchar('0' + (num / div));
-			num %= div;
+			count += _putchar('0' + (temp / div));
+			temp %= div;
 			div /= 10;
 		}
 	}
