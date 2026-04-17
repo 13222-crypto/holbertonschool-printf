1c870b181b9c0296ff78f636d2e6c9533d77cff2
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-17 23:17:04 +0000

    Fix conflicting types and add width support

--- _printf.c
+++ _printf.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _printf - custom printf with width support
- * @format: character string
- * Return: number of characters printed
+ * _printf - custom printf with width and modifiers
+ * @format: string
+ * Return: count
  */
 int _printf(const char *format, ...)
 {
@@ -19,39 +19,33 @@ int _printf(const char *format, ...)
 		{
 			i++;
 			width = 0;
-			/* استخراج الـ Width من النص */
-			while (format[i] >= '0' && format[i] <= '9')
-			{
-				width = (width * 10) + (format[i] - '0');
+			if (format[i] == '*') {
+				width = va_arg(args, int);
 				i++;
+			} else {
+				while (format[i] >= '0' && format[i] <= '9') {
+					width = (width * 10) + (format[i] - '0');
+					i++;
+				}
 			}
-			/* استخراج الـ Modifier */
 			mod = 0;
-			if (format[i] == 'l' || format[i] == 'h')
-			{
-				mod = format[i];
-				i++;
-			}
-			/* معالجة الحالات */
+			if (format[i] == 'l' || format[i] == 'h') { mod = format[i]; i++; }
+			
 			if (format[i] == 'd' || format[i] == 'i') count += print_int(args, mod, width);
 			else if (format[i] == 's') count += print_str(args, width);
-			else if (format[i] == 'c')
-			{
+			else if (format[i] == 'u') count += print_unsigned(args, mod, width);
+			else if (format[i] == 'c') {
+				int c_val = va_arg(args, int);
 				while (width > 1) { count += _putchar(' '); width--; }
-				count += _putchar(va_arg(args, int));
+				count += _putchar(c_val);
 			}
 			else if (format[i] == '%') count += _putchar('%');
-			else
-			{
+			else {
 				count += _putchar('%');
-				if (width) /* إعادة طباعة الأرقام كأنها نص عادي إذا لم نجد حرف تحويل */
-				{
-					/* كود إضافي هنا للتعامل مع الحالات غير المعروفة */
-				}
+				if (mod) count += _putchar(mod);
 				count += _putchar(format[i]);
 			}
-		}
-		else count += _putchar(format[i]);
+		} else count += _putchar(format[i]);
 		i++;
 	}
 	_putchar(-1);
--- print_functions.c
+++ print_functions.c
@@ -1,38 +1,39 @@
 #include "main.h"
 
 /**
- * print_int - prints integers with l and h modifiers
+ * print_int - prints integers with width and modifiers
  * @l: arguments
- * @mod: modifier (l, h, or 0)
+ * @mod: modifier
+ * @width: width
  * Return: count
  */
-int print_int(va_list l, char mod)
+int print_int(va_list l, char mod, int width)
 {
 	long int n;
-	unsigned long int num;
+	unsigned long int num, temp;
 	unsigned long int div = 1;
-	int count = 0;
+	int count = 0, len = 0;
 
-	if (mod == 'l')
-		n = va_arg(l, long int);
-	else if (mod == 'h')
-		n = (short int)va_arg(l, int);
-	else
-		n = va_arg(l, int);
+	if (mod == 'l') n = va_arg(l, long int);
+	else if (mod == 'h') n = (short int)va_arg(l, int);
+	else n = va_arg(l, int);
 
-	if (n < 0)
-	{
-		count += _putchar('-');
-		num = n * -1;
+	if (n < 0) { len++; temp = n * -1; }
+	else temp = n;
+
+	if (temp == 0) len++;
+	else {
+		unsigned long int t2 = temp;
+		while (t2 > 0) { t2 /= 10; len++; }
 	}
-	else
-		num = n;
 
-	while (num / div > 9)
-		div *= 10;
+	while (width > len) { count += _putchar(' '); width--; }
 
-	while (div != 0)
-	{
+	if (n < 0) { count += _putchar('-'); num = n * -1; }
+	else num = n;
+
+	while (num / div > 9) div *= 10;
+	while (div != 0) {
 		count += _putchar('0' + (num / div));
 		num %= div;
 		div /= 10;
@@ -41,20 +42,19 @@ int print_int(va_list l, char mod)
 }
 
 /**
- * print_str - prints a string
- * @l: va_list arguments
- * Return: number of characters printed
+ * print_str - prints string with width
+ * @l: va_list
+ * @width: width
+ * Return: count
  */
-int print_str(va_list l)
+int print_str(va_list l, int width)
 {
 	char *s = va_arg(l, char *);
-	int i, count = 0;
+	int i, len = 0, count = 0;
 
-	if (!s)
-		s = "(null)";
-
-	for (i = 0; s[i]; i++)
-		count += _putchar(s[i]);
-
+	if (!s) s = "(null)";
+	while (s[len]) len++;
+	while (width > len) { count += _putchar(' '); width--; }
+	for (i = 0; s[i]; i++) count += _putchar(s[i]);
 	return (count);
 }
