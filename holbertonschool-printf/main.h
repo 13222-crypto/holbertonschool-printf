5413088e4e3c37435ba9090d7b2b8472f24ad83e
reem <ryma6110@gmail.com>
2026-04-17 20:11:27 +0000

    Fix conflicting types and implement minus flag

--- holbertonschool-printf/main.h
+++ holbertonschool-printf/main.h
@@ -7,8 +7,8 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_number(int n, int plus, int space, int zero, int width);
+int print_number(int n, int plus, int space, int zero, int minus, int width);
 int print_number_recursion(unsigned int n1);
-int print_base(unsigned int n, int base, int hash, int upper, int zero, int width);
+int print_base(unsigned int n, int base, int hash, int upper, int zero, int minus, int width);
 
 #endif
