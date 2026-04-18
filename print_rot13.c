88d9827a1341a2741654645690a7645a89112515
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-18 18:48:58 +0000

    Fix all conflicting types and satisfy requirements for Task 11 and 15

--- print_rot13.c
+++ print_rot13.c
@@ -1,37 +1,16 @@
 #include "main.h"
-
-/**
- * print_rot13 - encodes a string using rot13 and prints it
- * @l: arguments list
- * @width: width (ignored for now)
- * @precision: precision (ignored for now)
- * Return: number of characters printed
- */
-int print_rot13(va_list l, int width, int precision)
-{
-	char *s = va_arg(l, char *);
-	int i, j, count = 0;
-	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	(void)width;
-	(void)precision;
-
-	if (s == NULL)
-		s = "(null)";
-
-	for (i = 0; s[i]; i++)
-	{
-		for (j = 0; in[j]; j++)
-		{
-			if (s[i] == in[j])
-			{
-				count += _putchar(out[j]);
-				break;
-			}
-		}
-		if (!in[j])
-			count += _putchar(s[i]);
-	}
-	return (count);
+int print_rot13(va_list l, int width, int precision) {
+    char *s = va_arg(l, char *);
+    int i, j, count = 0;
+    char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+    (void)width; (void)precision;
+    if (!s) s = "(null)";
+    for (i = 0; s[i]; i++) {
+        for (j = 0; in[j]; j++) {
+            if (s[i] == in[j]) { count += _putchar(out[j]); break; }
+        }
+        if (!in[j]) count += _putchar(s[i]);
+    }
+    return (count);
 }
