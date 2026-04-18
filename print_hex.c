afc13f60bc53540294cad3ba57d966017446a8c1
Bayadir Aldossari <13222@holbertonstudents.com>
2026-04-18 18:11:53 +0000

    Finish task 11: Correct return values and precision for u, o, x, X

--- print_hex.c
+++ print_hex.c
@@ -1,55 +1,43 @@
 #include "main.h"
 
-/**
- * handle_hex - helper for hex printing
- */
-int handle_hex(unsigned long int n, char *hex, int width, int precision)
+int handle_hex(unsigned long int n, char *hex, int width, int prec)
 {
-	char a[16];
-	int i = 0, j, count = 0, len = 0;
+	int a[20], i = 0, count = 0, len = 0, zeros = 0;
 	unsigned long int temp = n;
 
-	if (temp == 0) len = 1;
+	if (n == 0 && prec != 0) len = 1;
 	else while (temp > 0) { temp /= 16; len++; }
-	
-	/* معالجة الـ precision في الأرقام: إذا كانت أكبر من الطول، تزيد الأصفار */
-	if (precision > len) len = precision;
 
-	while (width > len) { count += _putchar(' '); width--; }
-	
-	/* إضافة الأصفار الناتجة عن الـ precision */
-	while (precision > i + (n == 0 ? 0 : 0)) {
-		/* هذا الجزء يحتاج منطق أدق ولكن لإصلاح التعارض الآن: */
-		break; 
+	if (prec > len) zeros = prec - len;
+	if (n == 0 && prec == 0) len = 0;
+
+	while (width > (len + zeros)) { count += _putchar(' '); width--; }
+	while (zeros-- > 0) count += _putchar('0');
+
+	if (len > 0) {
+		if (n == 0) count += _putchar('0');
+		else {
+			while (n > 0) { a[i++] = n % 16; n /= 16; }
+			for (i--; i >= 0; i--) count += _putchar(hex[a[i]]);
+		}
 	}
-
-	if (n == 0 && precision != 0) return (count + _putchar('0'));
-	if (n == 0 && precision == 0) return (count);
-
-	while (n > 0) {
-		a[i++] = hex[n % 16];
-		n /= 16;
-	}
-	for (j = i - 1; j >= 0; j--)
-		count += _putchar(a[j]);
-
 	return (count);
 }
 
-int print_hex(va_list l, char mod, int width, int precision)
+int print_hex(va_list l, char mod, int width, int prec)
 {
 	unsigned long int n;
 	if (mod == 'l') n = va_arg(l, unsigned long int);
 	else if (mod == 'h') n = (unsigned short int)va_arg(l, unsigned int);
 	else n = va_arg(l, unsigned int);
-	return (handle_hex(n, "0123456789abcdef", width, precision));
+	return (handle_hex(n, "0123456789abcdef", width, prec));
 }
 
-int print_HEX(va_list l, char mod, int width, int precision)
+int print_HEX(va_list l, char mod, int width, int prec)
 {
 	unsigned long int n;
 	if (mod == 'l') n = va_arg(l, unsigned long int);
 	else if (mod == 'h') n = (unsigned short int)va_arg(l, unsigned int);
 	else n = va_arg(l, unsigned int);
-	return (handle_hex(n, "0123456789ABCDEF", width, precision));
+	return (handle_hex(n, "0123456789ABCDEF", width, prec));
 }
