b0683015917c24181d436fa7315ececa1f9947e1
reem <ryma6110@gmail.com>
2026-04-18 20:31:55 +0000

    Add support for %b binary conversion specifier

--- _printf.c
+++ _printf.c
@@ -8,6 +8,14 @@ int get_n_len(unsigned int n, int base)
 	return (len);
 }
 
+int print_binary(unsigned int n)
+{
+	int count = 0;
+	if (n / 2) count += print_binary(n / 2);
+	count += _putchar((n % 2) + '0');
+	return (count);
+}
+
 int print_rot13(char *s)
 {
 	int i, j, count = 0;
@@ -18,11 +26,7 @@ int print_rot13(char *s)
 	{
 		for (j = 0; in[j]; j++)
 		{
-			if (s[i] == in[j])
-			{
-				count += _putchar(out[j]);
-				break;
-			}
+			if (s[i] == in[j]) { count += _putchar(out[j]); break; }
 		}
 		if (!in[j]) count += _putchar(s[i]);
 	}
@@ -37,27 +41,22 @@ int print_base(unsigned int n, int base, int hash, int upper, int zero, int minu
 	if (precision >= 0) zero = 0;
 	p_len = (precision > len) ? precision : len;
 	if (hash && n != 0) p_len += (base == 8) ? 1 : 2;
-	if (!minus && !zero && width > p_len)
-		while (width > p_len++) count += _putchar(' ');
-	if (hash && n != 0)
-	{
+	if (!minus && !zero && width > p_len) while (width > p_len++) count += _putchar(' ');
+	if (hash && n != 0) {
 		count += _putchar('0');
 		if (base == 16) count += _putchar(upper ? 'X' : 'x');
 	}
-	if (zero && !minus && width > p_len)
-		while (width > p_len++) count += _putchar('0');
+	if (zero && !minus && width > p_len) while (width > p_len++) count += _putchar('0');
 	while (precision > len++) count += _putchar('0');
-	if (!(precision == 0 && n == 0))
-	{
-		unsigned int temp = n;
+	if (!(precision == 0 && n == 0)) {
 		if (n == 0) count += _putchar('0');
 		else {
+			unsigned int temp = n;
 			while (temp > 0) { buffer[i++] = set[temp % base]; temp /= base; }
 			while (--i >= 0) count += _putchar(buffer[i]);
 		}
 	}
-	if (minus && width > count)
-		while (width > count) count += _putchar(' ');
+	if (minus && width > count) while (width > count) count += _putchar(' ');
 	return (count);
 }
 
@@ -70,17 +69,14 @@ int print_number(int n, int plus, int space, int zero, int minus, int width, int
 	p_len = (precision > len) ? precision : len;
 	if (sign || plus || space) p_len++;
 	if (precision >= 0) zero = 0;
-	if (!minus && !zero && width > p_len)
-		while (width > p_len++) count += _putchar(' ');
+	if (!minus && !zero && width > p_len) while (width > p_len++) count += _putchar(' ');
 	if (sign) count += _putchar('-');
 	else if (plus) count += _putchar('+');
 	else if (space) count += _putchar(' ');
-	if (zero && !minus && width > p_len)
-		while (width > p_len++) count += _putchar('0');
+	if (zero && !minus && width > p_len) while (width > p_len++) count += _putchar('0');
 	while (precision > len++) count += _putchar('0');
 	if (!(precision == 0 && n == 0)) count += print_number_recursion(n1);
-	if (minus && width > count)
-		while (width > count) count += _putchar(' ');
+	if (minus && width > count) while (width > count) count += _putchar(' ');
 	return (count);
 }
 
@@ -113,17 +109,13 @@ int _printf(const char *format, ...)
 			plus = space = hash = zero = minus = width = 0; precision = -1; i++;
 			while (format[i] == '+' || format[i] == ' ' || format[i] == '#' || format[i] == '0' || format[i] == '-')
 			{
-				if (format[i] == '+') plus = 1;
-				else if (format[i] == ' ') space = 1;
-				else if (format[i] == '#') hash = 1;
-				else if (format[i] == '0') zero = 1;
-				else if (format[i] == '-') minus = 1;
-				i++;
+				if (format[i] == '+') plus = 1; else if (format[i] == ' ') space = 1;
+				else if (format[i] == '#') hash = 1; else if (format[i] == '0') zero = 1;
+				else if (format[i] == '-') minus = 1; i++;
 			}
 			if (format[i] == '*') { width = va_arg(args, int); i++; }
 			else { while (format[i] >= '0' && format[i] <= '9') width = width * 10 + (format[i++] - '0'); }
-			if (format[i] == '.')
-			{
+			if (format[i] == '.') {
 				i++;
 				if (format[i] == '*') { precision = va_arg(args, int); i++; }
 				else { precision = 0; while (format[i] >= '0' && format[i] <= '9') precision = precision * 10 + (format[i++] - '0'); }
@@ -133,6 +125,7 @@ int _printf(const char *format, ...)
 			else if (format[i] == 'o') count += print_base(va_arg(args, unsigned int), 8, hash, 0, zero, minus, width, precision);
 			else if (format[i] == 'x') count += print_base(va_arg(args, unsigned int), 16, hash, 0, zero, minus, width, precision);
 			else if (format[i] == 'X') count += print_base(va_arg(args, unsigned int), 16, hash, 1, zero, minus, width, precision);
+			else if (format[i] == 'b') count += print_binary(va_arg(args, unsigned int));
 			else if (format[i] == 'c') count += _putchar(va_arg(args, int));
 			else if (format[i] == 's') {
 				char *s = va_arg(args, char *);
--- main.h
+++ main.h
@@ -12,5 +12,6 @@ int print_number_recursion(unsigned int n1);
 int print_base(unsigned int n, int base, int hash, int upper, int zero, int minus, int width, int precision);
 int print_rev(char *s);
 int print_rot13(char *s);
+int print_binary(unsigned int n);
 
 #endif
